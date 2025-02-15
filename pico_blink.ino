0ac5d600be18479bc7e12940840a3141b1249184
Hiromasa Uestuki <uetaroyh@ybb.ne.jp>
2025-02-15 20:34:36 +0900

    ３回点滅

--- pico_blink.ino
+++ pico_blink.ino
@@ -3,8 +3,11 @@ void setup() {
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH)；
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW)；
-    delay(100);
+    for (int i = 0; i < 3; i++) {
+        digitalWrite(LED_BUILTIN, HIGH)；
+        delay(100);
+        digitalWrite(LED_BUILTIN, LOW)；
+        delay(200)；
+    }
+    delay(1000)；
 }
